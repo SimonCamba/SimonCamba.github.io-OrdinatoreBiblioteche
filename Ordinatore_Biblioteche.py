20d0d6271d715ac0495aefad5fcd9edd87922018
Simone_Camba <133588327+SimonCamba@users.noreply.github.com>
2025-02-13 20:22:48 +0100

    Update Ordinatore_Biblioteche.py

--- Ordinatore_Biblioteche.py
+++ Ordinatore_Biblioteche.py
@@ -7,25 +7,21 @@ def leggiCSV(file):
 tabella=leggiCSV('anagrafica_biblioteche.csv')
 
 def lista_tipologie(var):
-    tipAmm=[]
-    for row in var[1:]:
-        tipAmm.append(row[3])
-    b=list(set(tipAmm))
-    return b
+    t=[row[3] for row in var[1:]]
+    tipologie=list(set(t))
+    return tipologie
 
-categorie=lista_tipologie(tabella)
+tipAmm=lista_tipologie(tabella)
 
-def filtroTipAmm(var,str):
-    nomefile="biblioteche_categoria_"+str+'.csv'
-    with open(nomefile, "w", newline='') as mycsv:
+def FileTipAmm(var,type):
+    nomefile = f"biblioteche_categoria_{type}.csv"
+    with open(nomefile, "w", newline="") as miocsv:
         for row in var:
-            if str in row:
-                scrittore = csv.writer(mycsv, delimiter=";")
+            if type in row:
+                scrittore = csv.writer(miocsv, delimiter=",")
                 scrittore.writerow(row)
-    righe=0
-    for row in nomefile:
-        righe+=1
-    print(f"Le biblioteche appartenenti alla tipologia amministrativa {str} sono: {righe}")
+    print(f"Le biblioteche appartenenti alla tipologia amministrativa {type} sono: {len(nomefile)}")
+FileTipAmm(tabella,"Enti locali")
 
-for el in categorie:
-    filtroTipAmm(tabella, el)
+for el in tipAmm:
+    FileTipAmm(tabella,el)
